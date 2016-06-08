5650576ed7ed4e9db51c8f483c3e2c030dd0274a
eliotmiller <eliotmiller@umsl.edu>
2016-06-08 10:44:57 -0400

    Bug fix
    Was plotting the lower triangle of the panels with the x&y axes flipped
    to what they should have been.

--- R/corrplotter.R
+++ R/corrplotter.R
@@ -5,13 +5,19 @@
 #' column of data, and the upper triangle summarizes the correlation.
 #'
 #' @param dataframe The object you want to pull data from to plot.
-#' @param colIDs Optional vector of column IDs or names you want to plot from dataframe.
+#' @param ids Optional vector of column IDs or names you want to plot from dataframe.
 #' @param hist.breaks The number of breaks desired in the diagonal histograms. Default is
 #' 10.
 #' @param hist.col The color of the histograms. Default is black.
 #' @param hist.names The titles to be printed above histograms, if desired. Will be left
 #' blank if not provided.
-#' @param text.cex The size of the summary stat text in the upper half of the plots.
+#' @param hist.names.cex The size of the hist.names titles. Defaults to 1.
+#' @param hist.names.spacing The space of the title below the top of the histogram plot.
+#' Default is -2. Increasing this number will move the title closer to the top of the
+#' panel.
+#' @param hist.y The y-axis label in the diagonal histograms. Defaults to blank, but can
+#' be set to, e.g., "Frequency".
+#' @param summary.cex The size of the summary stat text in the upper half of the plots.
 #' Default is 2.
 #' @param neg.color The background color of the panel in the upper half of the plots for
 #' correlations with negative coefficients. It is very important that this be formatted as
@@ -35,21 +41,24 @@
 #' want to print text along the last panel (the histogram), simply make the last element
 #' of the vector be equal to "".
 #' @param bottom.text Like left.text, but for the bottom axis labels.
+#' @param left.text.cex The size of the left.text labels. Defaults to 1.
+#' @param bottom.text.cex The size of the bottom.text labels. Defaults to 1.
 #' @param expansion Controls how much the histogram is shrunk down to facilitate plotting
 #' of histogram names. Also controls how zoomed out the scatterplots are, which might be
 #' useful for ensuring dots do not interfere with axis labels.
-#' @param ... Other arguments that can be passed to plot, including cex for the size of
-#' the dots in the scatterplot.
-#' @details This function sends out jobs to as many cores as are specified. Each 
-#' randomizes the input CDM according to all defined null models, then calculates each
-#' observed metric on each randomized matrix.
+#' @param ... Other arguments that can be passed to plot, including cex and color for the
+#' dots in the scatterplot.
+#'
+#' @details There are a large variety of options that can be customized on this function,
+#' but fundamentally it places scatterplots in the lower triangle of the plot, histograms
+#' along the diagram, and a summary of the correlation in the upper triangle.
 #'
 #' @return A paneled correlation plot.
 #'
 #' @export
 #'
 #' @importFrom grDevices colorRampPalette rgb
-#' @importFrom graphics hist mtext par plot rect text title
+#' @importFrom graphics hist mtext par plot rect text title axis
 #' @importFrom stats cor.test
 #'
 #' @examples
@@ -60,22 +69,22 @@
 #'
 #' corrplotter(dataframe=exData, hist.breaks=20, hist.col="purple",
 #'	hist.names=c("one","two","three"), left.text=c("units1", "units2", ""),
-#'	bottom.text=c("other_units1", "other_units2", ""),
-#'	text.cex=2, neg.color=c(1,0,0,1), mid.color=c(1,1,0,1), pos.color=c(0,1,0,1),
+#'	bottom.text=c("other_units1", "other_units2", "other_units3"),
+#'	summary.cex=2, neg.color=c(1,0,0,1), mid.color=c(1,1,0,1), pos.color=c(0,1,0,1),
 #'	expansion=0.5, cex=0.5, col="gray")
 
-corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
-	text.cex, neg.color, mid.color, pos.color, par.oma, par.tcl, par.mgp, par.lab,
-	left.text, bottom.text, expansion, ...)
+corrplotter <- function(dataframe, ids, hist.breaks, hist.col, hist.names,
+	hist.names.cex, hist.names.spacing, hist.y, summary.cex, neg.color, mid.color,
+	pos.color, par.oma, par.tcl, par.mgp, par.lab, left.text, bottom.text, left.text.cex,
+	bottom.text.cex, expansion, ...)
 {
-	#pull out only the columns you want to plot
-	if(missing(colIDs))
+	if(missing(ids))
 	{
-		colIDs <- 1:dim(dataframe)[2]
+		dataframe <- dataframe
 	}
 	else
 	{
-		colIDs <- colIDs
+		dataframe <- dataframe[,ids]
 	}
 
 	#fill in all the missing graphical parameters with the defaults
@@ -142,6 +151,33 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 		hist.names <- hist.names
 	}
 
+	if(missing(hist.names.cex))
+	{
+		hist.names.cex <- 1
+	}
+	else
+	{
+		hist.names.cex <- hist.names.cex
+	}
+
+	if(missing(hist.names.spacing))
+	{
+		hist.names.spacing <- -2
+	}
+	else
+	{
+		hist.names.spacing <- hist.names.spacing
+	}
+
+	if(missing(hist.y))
+	{
+		hist.y <- ""
+	}
+	else
+	{
+		hist.y <- hist.y
+	}
+
 	if(missing(left.text))
 	{
 		left.text <- rep("", dim(dataframe)[2])
@@ -158,6 +194,24 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 	else
 	{
 		bottom.text <- bottom.text
+	}
+
+	if(missing(left.text.cex))
+	{
+		left.text.cex <- 1
+	}
+	else
+	{
+		left.text.cex <- left.text.cex
+	}
+
+	if(missing(bottom.text.cex))
+	{
+		bottom.text.cex <- 1
+	}
+	else
+	{
+		bottom.text.cex <- bottom.text.cex
 	}
 
 	if(missing(neg.color))
@@ -187,13 +241,13 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 		pos.color <- pos.color
 	}
 	
-	if(missing(text.cex))
+	if(missing(summary.cex))
 	{
-		text.cex <- 2
+		summary.cex <- 2
 	}
 	else
 	{
-		text.cex <- text.cex
+		summary.cex <- summary.cex
 	}
 	
 	if(missing(expansion))
@@ -243,8 +297,8 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 			#plot the upper triangle
 			if(j > i)
 			{
-				#create a temporary correlation of column j as a function of i
-				temp <- cor.test(x=dataframe[,i], y=dataframe[,j])
+				#create a temporary correlation of column i as a function of j
+				temp <- cor.test(x=dataframe[,j], y=dataframe[,i])
 				
 				#find the interval the corr coefficient p-value belongs to
 				rectColor <- names(corColors[findInterval(temp$estimate, corColors)])
@@ -257,7 +311,7 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 				#add the correlation coefficient, rounded to 2 digits, and the
 				#significance on the next line. note the "\n" to start a newline
 				text(x=1, y=1, paste(expression("  r ="), round(temp$estimate, 2), "\n",
-					paste(expression("p ="), round(temp$p.value, 2))), cex=text.cex)
+					paste(expression("p ="), round(temp$p.value, 2))), cex=summary.cex)
 			}        
 			#plot the diagonals
 			if(j == i)
@@ -267,19 +321,21 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 				tempHist <- hist(dataframe[,j], breaks=hist.breaks, plot=FALSE)
 				hist(dataframe[,j], main="", breaks=hist.breaks, col=hist.col,
 					ylim=c(0, max(tempHist$counts) * (1 + expansion)))
-				title(hist.names[i], line=-2)
+				axis(side=2, labels=hist.y, tick=FALSE,
+					at=max(tempHist$counts) * (1 + expansion)/2, line=-1.5)
+				title(hist.names[i], line=hist.names.spacing, cex.main=hist.names.cex)
 			}
 			#plot the lower triangle
 			if(j < i)
 			{
 				#first calculate the range between the min and max for x and for y
-				xRange <- diff(range(dataframe[,i]))
-				yRange <- diff(range(dataframe[,j]))
-				plot(dataframe[,j]~dataframe[,i],
-					xlim=c(	min(dataframe[,i]) - (xRange * expansion),
-							max(dataframe[,i]) + (xRange * expansion)),
-					ylim=c(	min(dataframe[,j]) - (yRange * expansion),
-							max(dataframe[,j]) + (yRange * expansion)), ...)
+				xRange <- diff(range(dataframe[,j]))
+				yRange <- diff(range(dataframe[,i]))
+				plot(dataframe[,i]~dataframe[,j],
+					xlim=c(	min(dataframe[,j]) - (xRange * expansion),
+							max(dataframe[,j]) + (xRange * expansion)),
+					ylim=c(	min(dataframe[,i]) - (yRange * expansion),
+							max(dataframe[,i]) + (yRange * expansion)), ...)
 			}
 		}
 	}
@@ -291,8 +347,8 @@ corrplotter <- function(dataframe, colIDs, hist.breaks, hist.col, hist.names,
 	places <- places[1:dim(dataframe)[2]*2]
 	
 	#add the left side labels
-	mtext(text=left.text, at=places, line=2, side=2, outer=TRUE)
+	mtext(text=left.text, at=places, line=2, side=2, outer=TRUE, cex=left.text.cex)
 	
 	#add the bottom side labels if provided
-	mtext(text=bottom.text, at=places, line=2, side=1, outer=TRUE)
+	mtext(text=bottom.text, at=places, line=2, side=1, outer=TRUE, cex=bottom.text.cex)
 }
